fa81c309594b9b1991c3c5beef2e801270c4cc10
RobertChen998 <robertchen998@gamil.com>
2025-01-02 08:19:19 +0800

    second trail push

--- DLRM/run_dlrm_baseline_cpu_gpu.sh
+++ DLRM/run_dlrm_baseline_cpu_gpu.sh
@@ -1,12 +1,71 @@
-python dlrm_baseline_cpu_gpu.py --arch-sparse-feature-size=16 \
-								--arch-mlp-bot="13-512-256-64-16" \
-								--arch-mlp-top="512-256-1" \
-								--data-generation=dataset \
+# python dlrm_baseline_cpu_gpu.py --arch-sparse-feature-size=16 \
+# 								--arch-mlp-bot="13-512-256-64-16" \
+# 								--arch-mlp-top="512-256-1" \
+# 								--data-generation=dataset \
+# 								--data-set=kaggle \
+# 								--raw-data-file=./input/kaggle/train.txt \
+# 								--processed-data-file=./input/kaggle/kaggleAdDisplayChallenge_processed.npz \
+# 								--loss-function=bce \
+# 								--round-targets=True \
+# 								--mini-batch-size=1024 \
+# 								--print-freq=4096 \
+# 								--print-time \
+# 								--nepochs=0
+
+python dlrm_baseline_cpu_gpu.py --arch-sparse-feature-size=4 \
+								--arch-mlp-bot="13-8-8-8-4" \
+								--arch-mlp-top="512-8-1" \
+								--data-generation=random \
 								--data-set=kaggle \
 								--raw-data-file=./input/kaggle/train.txt \
 								--processed-data-file=./input/kaggle/kaggleAdDisplayChallenge_processed.npz \
 								--loss-function=bce \
 								--round-targets=True \
-								--mini-batch-size=1024 \
+								--mini-batch-size=4096 \
 								--print-freq=4096 \
-								--print-time
+								--print-time \
+								--nepochs=1
+
+
+# python dlrm_baseline_cpu_gpu.py --arch-sparse-feature-size=4 \
+# 								--arch-mlp-bot="13-8-8-8-4" \
+# 								--arch-mlp-top="512-8-1" \
+# 								--data-generation=random \
+# 								--data-set=kaggle \
+# 								--raw-data-file=./input/kaggle/train.txt \
+# 								--processed-data-file=./input/kaggle/kaggleAdDisplayChallenge_processed.npz \
+# 								--loss-function=bce \
+# 								--round-targets=True \
+# 								--mini-batch-size=4096 \
+# 								--print-freq=4096 \
+# 								--print-time \
+# 								--nepochs=1
+
+# python dlrm_baseline_cpu_gpu.py --arch-sparse-feature-size=4 \
+# 								--arch-mlp-bot="13-8-8-8-4" \
+# 								--arch-mlp-top="512-8-1" \
+# 								--data-generation=random \
+# 								--data-set=kaggle \
+# 								--raw-data-file=./input/kaggle/train.txt \
+# 								--processed-data-file=./input/kaggle/kaggleAdDisplayChallenge_processed.npz \
+# 								--loss-function=bce \
+# 								--round-targets=True \
+# 								--mini-batch-size=4096 \
+# 								--print-freq=4096 \
+# 								--print-time \
+# 								--nepochs=5
+
+
+# python dlrm_baseline_cpu_gpu.py --arch-sparse-feature-size=4 \
+# 								--arch-mlp-bot="13-8-8-8-4" \
+# 								--arch-mlp-top="512-8-1" \
+# 								--data-generation=random \
+# 								--data-set=kaggle \
+# 								--raw-data-file=./input/kaggle/train.txt \
+# 								--processed-data-file=./input/kaggle/kaggleAdDisplayChallenge_processed.npz \
+# 								--loss-function=bce \
+# 								--round-targets=True \
+# 								--mini-batch-size=4096 \
+# 								--print-freq=4096 \
+# 								--print-time \
+# 								--nepochs=1
